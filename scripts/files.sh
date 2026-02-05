5d4a440084d061f5481f8c7eaf122c7dd92489c1
Jose Melgares <rjmzelaya@gmail.com>
2026-02-05 10:26:52 +0100

    feat: adds short bash scripts

---
+++ scripts/files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Script for changing file extensions"
+FOLDER=$1
+
+pushd $FOLDER
+ls -1 | while read line; do
+fileName="`echo $line | cut -d/. -f1`"
+fileExtension="`echo $line | cut -d/. -f2`"
+done
